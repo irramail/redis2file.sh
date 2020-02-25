8d2641c633c884eee9e46480688f4150733e4eb5
irramail <irramail@gmail.com>
2020-02-26 01:22:48 +0700

    Update text2speech.sh

--- text2speech.sh
+++ text2speech.sh
@@ -6,9 +6,11 @@ text=`curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "m
 LENGTH=`echo $text | wc -c`
 if [ "$LENGTH" -gt "1"  ]
 then
-echo inside "$text" end
-say -v Milena "$text" -r 190 -o hi.wav --data-format=LEF32@22050
 
+voice=`curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "get_voice", "id":123, "params": [] }' 192.168.0.99 | sed "s/^..jsonrpc.........result...//" | sed "s/...id.:123.$//"`
+echo inside "$text" end
+say -v $voice "$text" -r 190 -o hi.wav --data-format=LEF32@22050
+#Samantha Milena
 echo -n '{"jsonrpc": "2.0", "method": "get_data_mp3", "id":1, "params": ["' > data.json
 base64 -i hi.wav > hi.b64
 cat hi.b64  | tr -d '\n'>> data.json
