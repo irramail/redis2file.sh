210dfb4a8820975e5d7663cc5b5f7c98271c54f4
irramail <irramail@gmail.com>
2020-04-10 17:52:39 +0700

    Update text2speech.sh

--- text2speech.sh
+++ text2speech.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 while :;
 do
-text=`curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "get_stext", "id":123, "params": [] }' 192.168.0.99 | sed "s/^..jsonrpc.........result...//" | sed "s/...id.:123.$//" | sed "s/|/. /g"`
+line=`curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "get_stext", "id":123, "params": [] }' video.p6way.net | sed "s/^..jsonrpc.........result...//" | sed "s/...id.:123.$//"`
+text=`echo "$line" | sed "s/---\[\[slnc ....\]\]\|//g" | sed "s/\|\|/|/g" | sed "s/---//g"| sed "s/ /^/g" | sed "s/|/. /g"`
+
+#lf=$'\n'; echo "$line" | sed "s/\|/\\$lf/g" | sed "s/ /^/g" | sed  "s/^---.*//g" | grep  -v "^$"> timing.txt
+
+for words in $text
+do
+  say -v milena -r 190 -o timing.wav --data-format=LEF32@22050 [[slnc 500]] $words [[slnc 500]]
+  sec=`mediainfo --Inform="Audio;%Duration%" timing.wav`
+  secbc=`echo "1000/$sec" | bc -l`
+  #echo $secbc $words
+done
 
 LENGTH=`echo $text | wc -c`
 if [ "$LENGTH" -gt "1"  ]
 then
 
-voice=`curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "get_voice", "id":123, "params": [] }' 192.168.0.99 | sed "s/^..jsonrpc.........result...//" | sed "s/...id.:123.$//"`
+voice=`curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "get_voice", "id":123, "params": [] }' video.p6way.net | sed "s/^..jsonrpc.........result...//" | sed "s/...id.:123.$//"`
 echo inside "$text" end
 say -v $voice "$text" -r 190 -o hi.wav --data-format=LEF32@22050
 #Samantha Milena
@@ -17,7 +28,7 @@ cat hi.b64  | tr -d '\n'>> data.json
 echo -n '"] }' >> data.json
 
 HIB64=`cat hi.b64`
-curl -X POST -H "Content-Type: application/json" -d @data.json 192.168.0.99
+curl -X POST -H "Content-Type: application/json" -d @data.json video.p6way.net
 
 
 fi
